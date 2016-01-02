97b0dd8de0c4a97d5d4262dc43eec72e4bc728f4
Happy <yangbai6644@163.com>
2016-01-02 16:04:49 +0800

    test

---
+++ 2016-1-2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+str = "hello world"
+print(not str)
+
+a = 1;b = a;a = 2
+
+print(a, b)
---
+++ demo.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'a hello world GUI example.'
+
+#--python2.x写法
+from Tkinter import *
+import tkMessageBox
+
+#--python3.x写法
+# from tkinter import *
+# import tkinter.messagebox as tkMessageBox
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.nameInput = Entry(self)
+        self.nameInput.pack()
+        self.alertButton = Button(self, text='Hello', command=self.hello)
+        self.alertButton.pack()
+
+    def hello(self):
+        name = self.nameInput.get() or 'world'
+        tkMessageBox.showinfo('Alert', 'Hello, %s' % name)
+
+app = Application()
+app.master.title('Hello World')
+# 主消息循环:
+app.mainloop()
