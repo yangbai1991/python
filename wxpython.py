7426f028a9374fb746cd98b784afaab455abf121
yangbai <yangbai6644@gmail.com>
2016-01-03 17:47:32 +0800

    gui

---
+++ wxpython.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import wx
+
+
+class Frame(wx.Frame):
+    def __init__(self, image, parent=None, id=-1, pos=wx.DefaultPosition, title='Hello, wxPython!'):
+        temp = image.ConvertToBitmap()
+        size = temp.GetWidth(), temp.GetHeight()
+        wx.Frame.__init__(self, parent, id, title, pos, size)
+        self.bmp = wx.StaticBitmap(parent=self, bitmap=temp)
+
+class App(wx.App):
+    def OnInit(self):
+        image = wx.Image('img/xijun.jpg', wx.BITMAP_TYPE_JPEG)
+        self.frame = Frame(image)
+        self.frame.Show()
+        self.SetTopWindow(self.frame)
+        return True
+
+def main():
+    app = App()
+    app.MainLoop()
+
+if __name__ == '__main__':
+     main()
+
